576e5a8ea13c85e3e01e8004fb675ef9659f50f8
ilyashirko <ilyashirko@gmail.com>
2022-02-06 20:31:23 +0300

    ili4

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,36 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datetime import timedelta, timezone
+
 from django.shortcuts import render
 
+from datacenter.models import Passcard, Visit
+from datacenter.storage_information_view import format_duration, get_duration
 
-def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+TZ_MSK = timezone(timedelta(hours=3))
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+def passcard_info_view(request, passcode, alarm_time = 3600):
+    passcard = Passcard.objects.get(passcode=passcode)
+
+    this_passcard_visits = []
+    for visit in Visit.objects.filter(passcard=passcard):
+        entered_at = visit.entered_at
+        try:
+            how_long_inside = visit.leaved_at - entered_at
+        except TypeError:
+            how_long_inside = get_duration(visit)
+        finally:
+            is_strange = False
+            if how_long_inside.total_seconds() >= alarm_time:
+                is_strange = True
+            
+            this_passcard_visits.append(
+                {
+                    'entered_at': entered_at,
+                    'duration': format_duration(how_long_inside),
+                    'is_strange': is_strange
+                }
+            )
+
+    
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,39 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datetime import timedelta, timezone
+
 from django.shortcuts import render
+from django.utils.timezone import localtime
+
+from datacenter.models import Visit
+
+TZ_MSK = timezone(timedelta(hours=3))
+
+
+def get_duration(visit):
+    entered_at = localtime(visit.entered_at, timezone=TZ_MSK)
+    duration = localtime(timezone=TZ_MSK).replace(microsecond=0) - entered_at
+    return duration
+
+
+def format_duration(duration):
+    duration = int(duration.total_seconds())
+    hours = duration // 3600
+    minutes = (duration - hours * 3600) // 60
+    if hours < 24:
+        return f'{hours}:{minutes}:00'
+    else:
+        return f'{hours // 24}::{hours % 24}:{minutes}:00'
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    still_there = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for person in still_there:
+        non_closed_visits.append({
+            'who_entered': person.passcard.owner_name,
+            'entered_at': localtime(person.entered_at, timezone=TZ_MSK),
+            'duration': format_duration(get_duration(person)),
+        })
+    
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
